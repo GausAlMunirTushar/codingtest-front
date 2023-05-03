2469d4f09d375a8bca45b3d435bdb7c19c68e101
Gaus Al Munir Tushar <tushar01910533370@gmail.com>
2023-05-03 12:54:33 +0600

    email validation match and toast-alert issue fixed

--- src/components/SubscriptionForm.js
+++ src/components/SubscriptionForm.js
@@ -21,25 +21,24 @@ export default function SubscriptionForm() {
       body: JSON.stringify({ email })
     }).then(res => res.text())
       .then(data => JSON.parse(`${data}`))
-      .then(data => hitToast(data.message, data.success ? 'success' : 'error'))
-      .catch(() => hitToast('Something went wrong. Please try again.', 'error'))
+      .then(data => hitToast(data.success ? 'success' : 'error', data.message))
+      .catch(() => hitToast('error', 'Something went wrong. Please try again.'))
 
     setAlertClass('');
   }
 
   const validate = (email) => {
-    if (email.trim(/^([a-zA-Z0-9_\-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/).match() == null) {
+    if (email.trim().match(/^([a-zA-Z0-9_\-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
       return false;
     } else if (email.trim() === '') {
       return false;
     }
-
     return true;
   }
 
   return (
     <form className="w-full flex-w flex-c-m validate-form" onSubmit={handleSubmit}>
-      <div ref={parentComp} className={"wrap-input100 validate-input where1 " + alertClass} data-validate="Valid email is required: user@email.domain">
+      <div ref={parentComp} className={"wrap-input100 validate-input where1 " + alertClass} data-validate="Please enter a valid email address.">
         <input className="input100 placeholder0 s2-txt2" type="text" name="email" placeholder="Enter Email Address" onChange={e => setEmail(e.target.value)} />
         <span className="focus-input100"></span>
       </div>
